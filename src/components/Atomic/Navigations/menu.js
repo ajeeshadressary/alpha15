793c349233a96d50f509f64a5946f10ee406128c
Ajeesh Adressary <M1039401@mindtree.com>
2017-05-16 12:24:50 +0530

    third time

--- src/components/Atomic/Navigations/menu.js
+++ src/components/Atomic/Navigations/menu.js
@@ -7,6 +7,10 @@ export default class Menu extends React.Component {
               
                 <div className="container" style={{padding:'5px'}}>
                     <div className="row">
+					    <div className="col-md-3"><Link to={'/"ariel_24hrs"/'}>Ariel24Hrs</Link></div> 
+					    <div className="col-md-3"><Link to={'/"ariel_topload"/'}>Ariel Topload</Link></div> 
+					    <div className="col-md-3"><Link to={'/"ariel_brilliant"/'}>Ariel Brilliant</Link></div> 
+					    <div className="col-md-3"><Link to={'/ariel_matic/'}>Ariel Matic</Link></div> 
                         <div className="col-md-3 "><Link to={'/about-us/'}>About Us</Link></div>
                         <div className="col-md-3"><Link to={'/contact-us/'}>Contact Us</Link></div>                        
                     </div>
